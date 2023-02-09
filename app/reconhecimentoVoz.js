4a414a54b8c01da1be7be9ae3c40cb7103b60e8f
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2023-02-09 13:49:41 -0300

    Exibindo mensagem na tela

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -3,3 +3,9 @@ window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = "pt-Br";
 recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(event) {
+     console.log(event.results[0][0].transcript);
+}
