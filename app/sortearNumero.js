160b44b64aa458c904263779f67d700c1cde9114
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2023-02-09 13:15:01 -0300

    Manipulando a função com o menor e maior número

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,9 +1,21 @@
-//console.log("Olá Mundo");
+// console.log("Olá Mundo");
 
+// Const com menores e maiores valores
+const menorValor = 1;
+const maiorValor = 1000;
+
+// Número ecreto será o retorno da função gerarNumeroAleatorio()
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio() {
-     return parseInt(Math.random() * 100);
+     // O resultado aqui será entre o menor e o maior, só que queremos que este número possa ser o maior, então acrescentamos + 1
+     return parseInt(Math.random() * maiorValor + 1);
 }
 
 console.log("Número Secreto:", numeroSecreto);
+
+const elementoMenorValor = document.getElementById("menorValor");
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById("maiorValor");
+elementoMaiorValor.innerHTML = maiorValor;
